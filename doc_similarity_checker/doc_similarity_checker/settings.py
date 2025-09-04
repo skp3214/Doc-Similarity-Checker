13fae3c13dc1c9dc16d2c3c1aeb5882d2d03f6b5
Sachin Prajapati <skprajapati3214@gmail.com>
2025-09-04 09:49:16 +0530

    Update settings.py: Set DEBUG to False and configure ALLOWED_HOSTS for production

--- doc_similarity_checker/doc_similarity_checker/settings.py
+++ doc_similarity_checker/doc_similarity_checker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7j&4=+(t!n)-o&v$9pfzb+v!2r&ps6z4+s&py0u@21%*r)^g89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['doc-similarity-checker.onrender.com']
 
 
 # Application definition
